f2d756f82d0e20e886bee4cfb2086807e71d4039
Martin LEKPA <lekpamartin@yahoo.fr>
2018-08-23 09:09:47 +0200

    Update check_oauth.sh

--- authentication/oauth/check_oauth.sh
+++ authentication/oauth/check_oauth.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
 
-AUTH_URL=""
 TOKEN_URL=""
 API_URL=""
-USERNAME="${1:-defaultvalue}"
-CLIENTID=''
-CLIENTSECRET=''
-REDIRECT_URI=''
+CLIENTID=""
+CLIENTSECRET=""
 SCOPE=""
 
-if [ "$TOKEN_URL"=="" ] || [ "$API_URL"=="" ] || [ "$CLIENTID"=="" ] || [ "$CLIENTSECRET"=="" ]; then
-	echo "Some variables are unset in the script"
-	exit 1
-fi
+echo -n "Login : "
+read -s USERNAME
+echo -e "\n"
 
 echo -n "Mot de passe de $USERNAME : "
 read -s PASSWORD
 echo -e "\n"
 
 grant_type='password'
-OUTPUT=`curl -s -X POST -d "grant_type=$grant_type&client_id=$CLIENTID&client_secret=$CLIENTSECRET&scope=$SCOPE&username=$USERNAME&password=$PASSWORD&redirect_uri=$REDIRECT_URI" $TOKEN_URL`
-#echo $OUTPUT
+OUTPUT=`curl -q -s -X POST -d "grant_type=$grant_type&client_id=$CLIENTID&client_secret=$CLIENTSECRET&scope=$SCOPE&username=$USERNAME&password=$PASSWORD&redirect_uri=$REDIRECT_URI" $TOKEN_URL`
 
 SCOPE=`echo $OUTPUT | cut -d'"' -f4`
+REFRESTOKEN=`echo $OUTPUT | cut -d'"' -f14`
 ACCESSTOKEN=`echo $OUTPUT | cut -d'"' -f18`
 
 echo -e "Les valeurs obtenues:\n\t- SCOPE\t\t: $SCOPE \n\t- ACCESSTOKEN\t: $ACCESSTOKEN\n"
 
 grant_type='tokeninfo'
-curl "$API_URL&grant_type=$grant_type&client_id=$CLIENTID&client_secret=$CLIENTSECRET&access_token=$ACCESSTOKEN&redirect_uri=$REDIRECT_URI"
+curl "$API_URL&access_token=$ACCESSTOKEN"
 
 echo -e "\n"
